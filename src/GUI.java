7aadb76392144b2744f481e5416b800b4ce47fd6
Jordan Thomson <jordan.thomson.2019@uni.strath.ac.uk>
2021-10-24 11:36:37 +0100

    Refactoring of GUI to split out methods Safe close threads when exiting application via windows X button Commenting added Use of ThreadGroups added

--- src/GUI.java
+++ src/GUI.java
@@ -11,26 +11,54 @@ public class GUI extends JFrame {
     private JTable table = new JTable();
     private DefaultTableModel model;
     private Object[][] threads;
-    private Thread refresh;
     private String filterGroup = "";
     private String filterName = "";
 
+    /**
+     * Constructor to create the GUI for the thread viewer
+     * @param threadr
+     */
     public GUI(Threadr threadr) {
         this.threadr = threadr;
 
-        JPanel panel = new JPanel();
-        panel.setLayout(new BorderLayout());
-        table.setDefaultEditor(Object.class, null);
-        table.setDragEnabled(false);
-        getTableModel(filterGroup, filterName);
-
-        JScrollPane scrollPane = new JScrollPane(table);
-        table.setPreferredScrollableViewportSize(new Dimension(500, 150));
-
-        panel.add(scrollPane);
         Container contentPane = getContentPane();
         contentPane.setLayout(new BorderLayout());
-        contentPane.add(panel, BorderLayout.CENTER);
+        contentPane.add(getThreadTablePanel(), BorderLayout.CENTER);
+        contentPane.add(getStopButtonPanel(),BorderLayout.EAST);
+        contentPane.add(getFilterPanel(),BorderLayout.SOUTH);
+        setTitle("Thread Viewer");
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent ev) {
+                threadr.safeCloseThreads(null);  // interrupt any running threads
+                dispose();  // stop the program when windows 'x' is clicked
+            }
+        });
+        setSize(new Dimension(600, 450));
+        setVisible(true);
+        createRefreshTask();
+
+    }
+
+    /**
+     * Create action listener and timer to attach to the Event Dispatch Thread to make it thread sage
+     */
+    private void createRefreshTask() {
+        ActionListener task = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                getTableModel(filterGroup, filterName);
+            }
+        };
+        Timer timer = new Timer(10000,task);
+        timer.setRepeats(true);
+        timer.start();
+    }
+
+    /**
+     * Create JPanel for button to action Thread interrupts
+     * @return JPanel
+     */
+    private JPanel getStopButtonPanel() {
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new BoxLayout(buttonPanel,BoxLayout.PAGE_AXIS));
         JButton stop = new JButton("Stop");
@@ -42,33 +70,32 @@ public class GUI extends JFrame {
             getTableModel(filterGroup, filterName);
         });
         buttonPanel.add(stop);
-        contentPane.add(buttonPanel,BorderLayout.EAST);
-        contentPane.add(filter(),BorderLayout.SOUTH);
-        setTitle("Thread Viewer");
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent ev) {
-                dispose();  // stop the program when windows 'x' is clicked
-            }
-        });
-        setSize(new Dimension(600, 450));
-        setVisible(true);
-
-        ActionListener task = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                getTableModel(filterGroup, filterName);
-            }
-        };
-        Timer timer = new Timer(10000,task);
-        timer.setRepeats(true);
-        timer.start();
-
-
-        //refresh.start();
-
+        return buttonPanel;
     }
 
-    public Component filter() {
+    /**
+     * Create JPanel for displaying details in a table
+     * @return JPanel
+     */
+    private JPanel getThreadTablePanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        table.setDefaultEditor(Object.class, null);
+        table.setDragEnabled(false);
+        getTableModel(filterGroup, filterName);
+
+        JScrollPane scrollPane = new JScrollPane(table);
+        table.setPreferredScrollableViewportSize(new Dimension(500, 150));
+
+        panel.add(scrollPane);
+        return panel;
+    }
+
+    /**
+     * Create JPanel for getting input to search Threads
+     * @return (Component) JPanel
+     */
+    public Component getFilterPanel() {
         JTextField groupFilter = new JTextField();
         groupFilter.addKeyListener(new KeyAdapter() {
             @Override
@@ -101,26 +128,11 @@ public class GUI extends JFrame {
         return filterPanel;
     }
 
-    public Thread makeRefresh(){
-        return new Thread("RefreshThread") {
-            public void run() {
-                boolean running = true;
-                while (running) {
-                    try {
-                        Thread.sleep(5000);
-                        getTableModel(filterGroup, filterName);
-                    } catch(InterruptedException e) {
-                        this.interrupt();
-                        System.out.println("Refresh stopping");
-                        running = false;
-                    }
-                }
-                System.out.println("Refresh Stopped");
-            }
-        };
-    }
-
-
+    /**
+     * Generate the table model (fill it with thread info)
+     * @param groupFilter String to filter by ThreadGroup
+     * @param nameFilter String to filter by Thread name
+     */
     private void getTableModel(String groupFilter, String nameFilter) {
         try {
             table.clearSelection();
--- src/Threadr.java
+++ src/Threadr.java
@@ -4,25 +4,34 @@ public class Threadr {
 
     private Set<Thread> threadSet;
 
-
     public Threadr() {
         updateThreadSet();
-        createThread("my_thread_1");
-        createThread("kill_me");
-        createThread("Super Important");
-
+        ThreadGroup tg1 = new ThreadGroup("Example 1");
+        ThreadGroup tg2 = new ThreadGroup("Example 2");
+        ThreadGroup tg11 = new ThreadGroup("Example 1.1");
+        createThread("my_thread_1",tg1);
+        createThread("kill_me",tg2);
+        createThread("Super Important",tg11);
     }
 
+    /**
+     * Gets all threads as a set
+     */
     public void updateThreadSet() {
         threadSet = Collections.synchronizedSet(Thread.getAllStackTraces().keySet());
-
     }
 
+    /**
+     * Data transforms a Set<Thread> into an Object[][] to be processed as a table in a gui
+     * @param groupFilter String to be applied as filter to Thread.getThreadGroup()
+     * @param nameFilter String to be applied as filter to Thread.getName()
+     * @return Object[][] of Thread details
+     */
     public Object[][] getThreadArray(String groupFilter, String nameFilter) {
-
         updateThreadSet();
         int index = 0;
 
+        // use array list checking if the thread still exists and applying filters
         ArrayList<Thread> threadList = new ArrayList<>();
         for (Thread thread : threadSet) {
             if (Thread.getAllStackTraces().containsKey(thread)) {
@@ -33,6 +42,7 @@ public class Threadr {
             }
         }
 
+        // convert the arraylist to a 2d Object array
         final Object[][] threadArray = new Object[threadList.size()][6];
         for (Thread thread : threadList) {
             if (Thread.getAllStackTraces().containsKey(thread)) {
@@ -46,30 +56,15 @@ public class Threadr {
                 index++;
             }
         }
-
-
-        /*final Object[][] threadArray = new Object[threadSet.size()][6];
-
-        for (Thread thread : threadSet) {
-            if (Thread.getAllStackTraces().containsKey(thread)) {
-                if (thread.getThreadGroup().getName().toLowerCase().startsWith(groupFilter) || groupFilter.equals("")) {
-                    threadArray[index][0] = thread.getThreadGroup().getName();
-                    threadArray[index][1] = thread.getName();
-                    threadArray[index][2] = thread.getState();
-                    threadArray[index][3] = thread.getPriority();
-                    threadArray[index][4] = thread.getId();
-                    threadArray[index][5] = thread;
-
-                    index++;
-                }
-            }
-        }*/
-        System.out.println("num items = " + index);
-        Sort2DArrayBasedOnColumnNumber(threadArray, 0);
-
+        Sort2DArrayBasedOnColumnNumber(threadArray, 0);  // sort by column
         return threadArray;
     }
 
+    /**
+     * Helper method to sort a 2d array by column values
+     * @param array 2d array to be sorted
+     * @param column int index of column to sort by
+     */
     public void Sort2DArrayBasedOnColumnNumber(Object[][] array, int column) {
         Arrays.sort(array, new Comparator<Object[]>() {
             @Override
@@ -94,6 +89,10 @@ public class Threadr {
         });
     }
 
+    /**
+     * Attempt to interrupt a thread
+     * @param selected Thread being chosen for interrupt
+     */
     public void interruptThread(Object selected) {
         if (selected instanceof Thread) {
             threadSet.remove(selected);
@@ -101,8 +100,16 @@ public class Threadr {
         }
     }
 
-    public void createThread(String name) {
-        Thread t = new Thread(name) {
+    /**
+     * Helper method to create a thread for a ThreadGroup
+     * @param name String name of the Thread
+     * @param tg ThreadGroup to associate with the thread, null will use the current ThreadGroup
+     */
+    public void createThread(String name, ThreadGroup tg) {
+        if (tg == null) {
+            tg = Thread.currentThread().getThreadGroup();
+        }
+        Thread t = new Thread(tg,name) {
             public void run() {
                 boolean running = true;
                 while (running) {
@@ -114,7 +121,7 @@ public class Threadr {
                         running = false;
                     }
                 }
-                System.out.println(this.getName() + "Stopped!!");
+                System.out.println(this.getName() + " Stopped!!");
             }
         };
         t.start();
@@ -123,4 +130,27 @@ public class Threadr {
     public static void main(String[] args) {
         new GUI(new Threadr());
     }
+
+    /**
+     * Method to iterate over all threads and attempt to interrupt to allow them to safely close
+     * @param tg ThreadGroup to iterate over, if none supplied then root is chosen and iterates from there.
+     */
+    public void safeCloseThreads(ThreadGroup tg) {
+        if (tg == null) {
+            tg = Thread.currentThread().getThreadGroup();
+        }
+        int num_threads = tg.activeCount();
+        int num_groups = tg.activeGroupCount();
+        Thread[] threads = new Thread[num_threads];
+        ThreadGroup[] groups = new ThreadGroup[num_groups];
+        tg.enumerate(threads,false);
+        tg.enumerate(groups, false);
+        for (int i = 0; i < num_groups; i++) {
+            safeCloseThreads(groups[i]);
+        }
+        for (int i = 0; i < num_threads - num_groups; i++) {
+            threads[i].interrupt();
+        }
+    }
+
 }
